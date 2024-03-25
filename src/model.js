4763c096401f8bac9713943d7d5fc85f0a30fe2b
wsxhjnb1 <1057334122xie@gmail.com>
2024-03-24 23:07:44 -0400

    change for model

--- src/model.js
+++ src/model.js
@@ -14,13 +14,13 @@ async function getChatCompletion({ prompt, context }) {
         content: `
         You are a knowledgebase oracle. You are given a question and a context. You answer the question based on the context.
         Analyse the information from the context and draw fundamental insights to accurately answer the question to the best of your ability.
+
         Context: ${context}
       `,
       },
       { role: "user", content: prompt },
     ],
   });
-
   return chatCompletion.data.choices[0].message;
 }
 
